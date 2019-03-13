0a313d6fda2b051e6a84f960eb82f773c5af690f
Duncan Campbell <duncan@donut-hole.lan>
2019-03-12 22:15:43 -0400

    added tng300-1

--- simulation_props.py
+++ simulation_props.py
@@ -16,5 +16,12 @@ d_1_dmo = {'basePath': '/Volumes/G-RAID/simulations/unprocessed/Illustris/Illust
            'Lbox': np.array([75.0]*3)
            }
 
+d_2 = {'basePath': '/Volumes/G-RAID/simulations/unprocessed/Illustris/TNG300-1',
+       'm_dm': 0.00398342749867548*10**6.0,
+       'litte_h': 0.6774,
+       'Lbox': np.array([205.00]*3)
+       }
+
 sim_prop_dict = {'Illustris-1': d_1,
-                 'Illustris-1-Dark': d_1_dmo}
+                 'Illustris-1-Dark': d_1_dmo,
+                 'TNG300-1': d_2}
