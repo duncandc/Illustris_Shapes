fe4763f3451bef838e624864025e57c4756c637c
Duncan Campbell <duncan@donut-hole.fios-router.home>
2019-03-20 14:13:40 -0400

    workiong on vagc

--- make_vagc.py
+++ make_vagc.py
@@ -5,6 +5,8 @@ script to measure galaxy shapes in the Illustris simmulations
 from __future__ import print_function, division
 import numpy as np
 from astropy.table import Table, Column
+from astropy.io import ascii
+import sys
 
 from simulation_props import sim_prop_dict
 
@@ -54,49 +56,48 @@ def main():
     Ngals = len(gal_ids)
     print("number of galaxies in selection: {0}".format(Ngals))
 
-    fields = ['SubhaloGrNr', 'SubhaloMassInRadType', 'SubhaloPos', 'SubhaloVel']
+    fields = ['SubhaloGrNr', 'SubhaloMassInRadType', 'SubhaloMassType', 'SubhaloPos', 'SubhaloVel']
 
     galaxy_table = loadSubhalos(basePath, snapNum, fields=fields)
     
     # FoF host halo ID
-    host_halo_id = galaxy_table['SubhaloGrNr'][halo_ids]
+    host_halo_ids = galaxy_table['SubhaloGrNr'][gal_ids]
     
-    x = galaxy_table['SubhaloPos'][:,0][halo_ids]
-    y = galaxy_table['SubhaloPos'][:,1][halo_ids]
-    z = galaxy_table['SubhaloPos'][:,2][halo_ids]
+    x = galaxy_table['SubhaloPos'][:,0][gal_ids]
+    y = galaxy_table['SubhaloPos'][:,1][gal_ids]
+    z = galaxy_table['SubhaloPos'][:,2][gal_ids]
 
-    vx = galaxy_table['SubhaloVel'][:,0][halo_ids]
-    vy = galaxy_table['SubhaloVel'][:,1][halo_ids]
-    vz = galaxy_table['SubhaloVel'][:,2][halo_ids]
+    vx = galaxy_table['SubhaloVel'][:,0][gal_ids]
+    vy = galaxy_table['SubhaloVel'][:,1][gal_ids]
+    vz = galaxy_table['SubhaloVel'][:,2][gal_ids]
 
-    mstar_in_twice_halfrad = galaxy_table['SubhaloMassInRadType'][:,4][halo_ids]
+    mstar_in_twice_halfrad = galaxy_table['SubhaloMassInRadType'][:,4][gal_ids]
     mstar_in_twice_halfrad = mstar_in_twice_halfrad*10**10
 
-    mstar_all = galaxy_table['SubhaloMassType'][:,4][halo_ids]
+    mstar_all = galaxy_table['SubhaloMassType'][:,4][gal_ids]
     mstar_all = mstar_all*10**10
 
 
     # central galaxy ID of group
     fields = ['GroupFirstSub', 'Group_M_Mean200', 'Group_R_Mean200', 'GroupMass']
     host_halo_table = loadHalos(basePath, snapNum, fields=fields)
-    central_id = host_halo_table['GroupFirstSub'][host_halo_id]
+    central_id = host_halo_table['GroupFirstSub'][host_halo_ids]
 
     # host halo properties
-    host_halo_mass_200m = host_halo_table['Group_M_Mean200'][host_halo_id]
-    host_halo_radius_200m = host_halo_table['Group_R_Mean200'][host_halo_id]
-    host_halo_fof_mass = host_halo_table['GroupMass'][host_halo_id]
+    host_halo_mass_200m = host_halo_table['Group_M_Mean200'][host_halo_ids]
+    host_halo_radius_200m = host_halo_table['Group_R_Mean200'][host_halo_ids]
+    host_halo_fof_mass = host_halo_table['GroupMass'][host_halo_ids]
 
- 
-    # save measurements
+    # save table
     fpath = './data/value_added_catalogs/'
     fname = sim_name + '_' + str(snapNum) + '_vagc.dat'
     ascii.write([gal_ids, host_halo_ids, central_id,
     	         x, y, z,
-    	         vx, vy, vz
+    	         vx, vy, vz,
     	         mstar_in_twice_halfrad, mstar_all,
     	         host_halo_mass_200m, host_halo_radius_200m, host_halo_fof_mass],
                 fpath+fname,
-                ['gal_id', 'host_halo_id', 'central_id',
+                names=['gal_id', 'host_halo_id', 'central_id',
                  'x', 'y', 'z',
                  'vx', 'vy', 'vz',
                  'stellar_mass_in_twice_halfrad', 'stellar_mass_all',
