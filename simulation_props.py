506a0f2bf5607826b2e491f933428b3ab6bcff5e
Duncan Campbell <duncan.campbell@yale.edu>
2019-03-27 17:42:58 -0400

    changed to use base save path

--- simulation_props.py
+++ simulation_props.py
@@ -2,33 +2,41 @@
 dictionaries of Illustris simulation properties
 """
 
+from project_settings import base_savepath
+
 import numpy as np
 
-d_1 = {'basePath':'/Volumes/G-RAID/simulations/unprocessed/Illustris/Illustris-1',
+d_1 = {'basePath': base_savepath + 'Illustris-1',
        'm_dm': 6.3*10**6.0,
        'litte_h': 0.704,
        'Lbox': np.array([75.0]*3)
        }
 
-d_1_dmo = {'basePath': '/Volumes/G-RAID/simulations/unprocessed/Illustris/Illustris-1-Dark',
+d_1_dmo = {'basePath': base_savepath + 'Illustris-1-Dark',
            'm_dm': 6.3*10**6.0,
            'litte_h': 0.704,
            'Lbox': np.array([75.0]*3)
            }
 
-d_2 = {'basePath': '/Volumes/G-RAID/simulations/unprocessed/Illustris/TNG300-1/output',
-       'm_dm': 40.0*10**6.0,
+d_2 = {'basePath': base_savepath + 'TNG300-1/output',
+       'm_dm': 0.00398342749867548*10**10,
        'litte_h': 0.6774,
        'Lbox': np.array([205.00]*3)
        }
 
-d_3 = {'basePath': '/Volumes/G-RAID/simulations/unprocessed/Illustris/TNG100-1/output',
+d_2_dmo = {'basePath': base_savepath + 'TNG300-1-Dark/output',
+       'm_dm': 0.0047271638660809*10**10,
+       'litte_h': 0.6774,
+       'Lbox': np.array([205.00]*3)
+       }
+
+d_3 = {'basePath': base_savepath + 'TNG100-1/output',
        'm_dm': 0.000505574296436975*10**10,
        'litte_h': 0.6774,
        'Lbox': np.array([75.0]*3)
        }
 
-d_3_dmo = {'basePath': '/Volumes/G-RAID/simulations/unprocessed/Illustris/TNG100-1-Dark/output',
+d_3_dmo = {'basePath': base_savepath + 'TNG100-1-Dark/output',
        'm_dm': 0.000599968882709879*10**10,
        'litte_h': 0.6774,
        'Lbox': np.array([75.0]*3)
@@ -37,5 +45,6 @@ d_3_dmo = {'basePath': '/Volumes/G-RAID/simulations/unprocessed/Illustris/TNG100
 sim_prop_dict = {'Illustris-1': d_1,
                  'Illustris-1-Dark': d_1_dmo,
                  'TNG300-1': d_2,
+                 'TNG300-1-Dark': d_2_dmo,
                  'TNG100-1': d_3,
                  'TNG100-1-Dark': d_3_dmo}
