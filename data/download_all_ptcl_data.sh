06721cfe0ed87cf10ec7aba83f2d648a43409af7
Duncan Campbell <duncan.campbell@yale.edu>
2019-03-27 17:42:34 -0400

    modified readme

--- data/download_all_ptcl_data.sh
+++ data/download_all_ptcl_data.sh
@@ -3,7 +3,30 @@
 # full physics particle data
 python download_ptcl_data.py Illutris-1 135
 python download_ptcl_data.py Illutris-1 99
+python download_ptcl_data.py Illutris-1 85
 
 # dark matter only particle data
 python download_ptcl_data.py Illutris-1-Dark 135
 python download_ptcl_data.py Illutris-1-Dark 99
+python download_ptcl_data.py Illutris-1-Dark 85
+
+# full physics particle data
+python download_ptcl_data.py TNG100-1 99
+python download_ptcl_data.py TNG100-1 63
+python download_ptcl_data.py TNG100-1 50
+
+# dark matter only particle data
+python download_ptcl_data.py TNG100-1-Dark 99
+python download_ptcl_data.py TNG100-1-Dark 63
+python download_ptcl_data.py TNG100-1-Dark 50
+
+# full physics particle data
+python download_ptcl_data.py TNG300-1 99
+python download_ptcl_data.py TNG300-1 63
+python download_ptcl_data.py TNG300-1 50
+
+# dark matter only particle data
+python download_ptcl_data.py TNG300-1-Dark 99
+python download_ptcl_data.py TNG300-1-Dark 63
+python download_ptcl_data.py TNG300-1-Dark 50
+
